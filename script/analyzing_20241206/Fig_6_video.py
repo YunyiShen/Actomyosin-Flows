e7fd4d08e3cc541f538577013ec73faa143a3174
Yunyi Shen <yshen99@mit.edu>
2026-05-06 18:54:12 -0400

    align time

--- script/analyzing_20241206/Fig_6_video.py
+++ script/analyzing_20241206/Fig_6_video.py
@@ -78,30 +78,33 @@ for drag_range in [
     #u[chi > 0.8] = np.nan
     nx, ny = N, N
     n_frame = len(t)
-    n_plot_time_series = 60
-    u_ts = u[::(n_frame//n_plot_time_series)]
-    v_ts = v[::(n_frame//n_plot_time_series)]
-    stress_ext_ts = stress_ext_save[::(n_frame//n_plot_time_series)]
+    n_plot_time_series = 25
+    u_ts = u[:81]#[::(n_frame//n_plot_time_series)]
+    v_ts = v[:81]#[::(n_frame//n_plot_time_series)]
+    stress_ext_ts = stress_ext_save[:81]#[::(n_frame//n_plot_time_series)]
     #breakpoint()
-    t_ts = t[::(n_frame//n_plot_time_series)]
+    t_ts = t[:81]#[::(n_frame//n_plot_time_series)]
     
     # fig to hold frames
-    fig = plt.figure(figsize=(3*3.5, 9.0))
-    gs = gridspec.GridSpec(3, 5, width_ratios=[1, 1, 0.03, 1, 0.03])
-    ax0 = fig.add_subplot(gs[0, 0])
-    ax1 = fig.add_subplot(gs[0, 1])
-    cax1 = fig.add_subplot(gs[0, 2])
-    ax2 = fig.add_subplot(gs[0, 3])
-    ax3 = fig.add_subplot(gs[1, 1])
-    cax2 = fig.add_subplot(gs[1, 2])
-    ax4 = fig.add_subplot(gs[1, 3])
-    ax5 = fig.add_subplot(gs[2, 1])
-    cax3 = fig.add_subplot(gs[0, 4])
-    cax4 = fig.add_subplot(gs[2, 2])
-    cax5 = fig.add_subplot(gs[1, 4])
-    ax6 = fig.add_subplot(gs[2, 3])
-    cax6 = fig.add_subplot(gs[2, 4])
+    fig = plt.figure(figsize=(3*3.5, 3.0))
+    gs = gridspec.GridSpec(1, 6, width_ratios=[1, 0.03, 1, 0.03, 1, 0.03])
+    #ax0 = fig.add_subplot(gs[0, 0])
+    ax1 = fig.add_subplot(gs[0, 0])
+    cax1 = fig.add_subplot(gs[0, 1])
+    #ax2 = fig.add_subplot(gs[0, 3])
+    #ax3 = fig.add_subplot(gs[1, 1])
+    #cax2 = fig.add_subplot(gs[1, 2])
+    #ax4 = fig.add_subplot(gs[1, 3])
+    ax5 = fig.add_subplot(gs[0, 2])
+    #cax3 = fig.add_subplot(gs[0, 4])
+    cax4 = fig.add_subplot(gs[0, 3])
+    #cax5 = fig.add_subplot(gs[1, 4])
+    ax6 = fig.add_subplot(gs[0, 4])
+    cax6 = fig.add_subplot(gs[0, 5])
     camera = Camera(fig)
+    
+    
+    #breakpoint()
     for tt in tqdm(range(u_ts.shape[0])):
         U = u_ts[tt]
         V = v_ts[tt]
@@ -144,19 +147,6 @@ for drag_range in [
         u_f_masked = np.ma.array(u_f, mask = mask)
         v_f_masked = np.ma.array(v_f, mask = mask)
     
-    
-    
-
-        cf = ax0.contourf(Xf, Yf, stress_masked, 
-                  corner_mask=True, antialiased=True,
-            levels=np.linspace(0, stress_max/1000,  21), 
-            cmap='viridis')
-
-        ax0.set_xticks([])
-        ax0.set_yticks([])
-        ax0.set_axis_off()
-        ax0.set_aspect('equal')
-        ax0.set_title("stress")
 
 
         ## overlay 
@@ -181,7 +171,7 @@ for drag_range in [
         ax1.set_axis_off()
 
         ax1.set_aspect('equal')
-        ax1.set_title("overlay")  
+        ax1.set_title("Velocity Overlaid")  
 
 
         import matplotlib
@@ -197,92 +187,10 @@ for drag_range in [
         sm = matplotlib.cm.ScalarMappable(norm=norm, cmap="viridis")
         sm.set_array([])  # required by colorbar
         cbar = fig.colorbar(sm, cax=cax1, ticks=np.linspace(0, stress_max/1000, 6))
-        cbar.set_label('Contractile stress by actomyosin (Pa)') 
+        cbar.set_label('Contractile stress (Pa)') 
 
     
-        ## vel 
-        
-        cf = ax2.contourf(Xf, Yf, vel_masked * 1000 * 60, 
-                  corner_mask=True, antialiased=True,
-                  vmin=0,
-                  vmax=np.nanmax(vel_masked * 1000 * 60),
-            levels=np.linspace(0, np.nanmax(vel_masked * 1000 * 60),  21), 
-            #linewidths=0,
-            cmap=cmap)
     
-        ax2.set_xticks([])
-        ax2.set_yticks([])
-        ax2.set_axis_off()
-
-        ax2.set_aspect('equal')
-        ax2.set_title("velocity")
-    
-        
-        #cbar = fig.colorbar(cf, cax=cax3, extend="both")
-        norm = matplotlib.colors.Normalize(vmin=0,
-                  vmax=np.nanmax(vel_masked * 1000 * 60))
-        sm = matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap)
-        sm.set_array([])  # required by colorbar
-        cbar = fig.colorbar(sm, cax=cax3, ticks=np.linspace(0, np.nanmax(vel_masked * 1000 * 60), 6))
-        cbar.set_label('Velocity (µm/min)') 
-    
-    
-        ########################
-        ###### x and y #########
-        ########################
-        ## vel 
-        
-        cf = ax3.contourf(Xf, Yf, u_sizef_masked * 1000 * 60, 
-                  corner_mask=True, antialiased=True,
-                  vmin=0,
-                  vmax=np.nanmax(vel_masked * 1000 * 60),
-            levels=np.linspace(0, np.nanmax(vel_masked * 1000 * 60),  21), 
-            #linewidths=0,
-            cmap=cmap)
-    
-        ax3.set_xticks([])
-        ax3.set_yticks([])
-        ax3.set_axis_off()
-
-        ax3.set_aspect('equal')
-        ax3.set_title("x velocity, size")
-    
-        
-        #cbar = fig.colorbar(cf, cax=cax2, extend="both")
-        norm = matplotlib.colors.Normalize(vmin=0,
-                  vmax=np.nanmax(vel_masked * 1000 * 60))
-        sm = matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap)
-        sm.set_array([])  # required by colorbar
-        cbar = fig.colorbar(sm, cax=cax2, ticks=np.linspace(0, np.nanmax(vel_masked * 1000 * 60), 6))
-        cbar.set_label('Velocity (µm/min)') 
-    
-        
-        cf = ax4.contourf(Xf, Yf, v_sizef_masked * 1000 * 60, 
-                  corner_mask=True, antialiased=True,
-                  vmin=0,
-                  vmax=np.nanmax(vel_masked * 1000 * 60),
-            levels=np.linspace(0, np.nanmax(vel_masked * 1000 * 60),  21), 
-            #linewidths=0,
-            cmap=cmap)
-    
-        ax4.set_xticks([])
-        ax4.set_yticks([])
-        ax4.set_axis_off()
-
-        ax4.set_aspect('equal')
-        ax4.set_title("y velocity, size")
-    
-        
-        #cbar = fig.colorbar(cf, cax=cax5, extend="both")
-        norm = matplotlib.colors.Normalize(vmin=0,
-                  vmax=np.nanmax(vel_masked * 1000 * 60))
-        sm = matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap)
-        sm.set_array([])  # required by colorbar
-        cbar = fig.colorbar(sm, cax=cax5, ticks=np.linspace(0, np.nanmax(vel_masked * 1000 * 60), 6))
-        cbar.set_label('Velocity (µm/min)')
-    
-        #ax0.set_xticks([])
-        #ax0.set_yticks([])
     
         ########### with sign ##########
     
@@ -302,7 +210,7 @@ for drag_range in [
         ax5.set_axis_off()
 
         ax5.set_aspect('equal')
-        ax5.set_title("x velocity")
+        ax5.set_title("x velocity (µm/min)")
     
         
         #cbar = fig.colorbar(cf, cax=cax4, extend="both")
@@ -312,7 +220,7 @@ for drag_range in [
         sm.set_array([])  # required by colorbar
         cbar = fig.colorbar(sm, cax=cax4, ticks=np.linspace(-np.nanmax(vel_masked * 1000 * 60), 
                                                             np.nanmax(vel_masked * 1000 * 60), 6))
-        cbar.set_label('Velocity (µm/min)') 
+        cbar.set_label('') 
     
         
         cf = ax6.contourf(Xf, Yf, v_f_masked * 1000 * 60, 
@@ -329,7 +237,7 @@ for drag_range in [
         ax6.set_axis_off()
 
         ax6.set_aspect('equal')
-        ax6.set_title("y velocity")
+        ax6.set_title("y velocity (µm/min)")
     
         
         #cbar = fig.colorbar(cf, cax=cax6, extend="both")
@@ -338,7 +246,7 @@ for drag_range in [
         sm = matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap)
         sm.set_array([])  # required by colorbar
         cbar = fig.colorbar(sm, cax=cax6, ticks=np.linspace(-np.nanmax(vel_masked * 1000 * 60), np.nanmax(vel_masked * 1000 * 60), 6))
-        cbar.set_label('Velocity (µm/min)')
+        cbar.set_label('')
     
     
     
@@ -347,4 +255,4 @@ for drag_range in [
         #fig.savefig(f"./Fig_6/modelcell2D_Stokes_maxstress{stress_max}_drag{drag_range[0]}-{drag_range[1]}_size{cell_radius}_visc{visc_range[0]}-{visc_range[1]}_dt{dt}_dx{dx}_N{N}_tmax{tmax}.pdf")
         camera.snap()
     animation = camera.animate()
-    animation.save(f"./Fig_6/modelcell2D_Stokes_maxstress{stress_max}_drag{drag_range[0]}-{drag_range[1]}_size{cell_radius}_visc{visc_range[0]}-{visc_range[1]}_dt{dt}_dx{dx}_N{N}_tmax{tmax}.mp4", writer='ffmpeg', fps=20)
+    animation.save(f"./Fig_6/modelcell2D_Stokes_maxstress{stress_max}_drag{drag_range[0]}-{drag_range[1]}_size{cell_radius}_visc{visc_range[0]}-{visc_range[1]}_dt{dt}_dx{dx}_N{N}_tmax{tmax}.mp4", writer='ffmpeg', fps=10, dpi = 500)
